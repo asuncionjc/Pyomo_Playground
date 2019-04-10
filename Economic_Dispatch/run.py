1d9cbd561f62155add4cc025b4bb0ff6b6aeab96
Asuncion Jimenez Cordero <asuncionjc@us.es>
2019-04-10 09:08:57 +0200

    Finish the first version of the script to execute

--- Economic_Dispatch/run.py
+++ Economic_Dispatch/run.py
@@ -9,9 +9,21 @@ from __future__ import division
 from pyomo.environ import *
 from pyomo.opt import SolverFactory
 
-# Este paquete te sirve para cambiar directorio
+# Extra packages
 import os
 
-# Ruta carpeta donde estás trabajando
+# Change the 
 path = 'C:/Users/Asun/Github/Pyomo_Playground/Economic_Dispatch'
 os.chdir(path)
+
+#Load the model
+
+from model import model
+
+solver = SolverFactory("cplex")
+
+path_instance = "/".join([path,"data.dat"])
+instance = model.create_instance(path_instance)
+
+results = opt.solve(instance,
+                    tee = True)
