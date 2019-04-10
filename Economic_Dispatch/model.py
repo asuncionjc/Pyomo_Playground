decd11a9170634ea5acd64a9bb9d008d53000226
Asuncion Jimenez Cordero <asuncionjc@us.es>
2019-04-10 10:35:01 +0200

    The constraints have been defined

--- Economic_Dispatch/model.py
+++ Economic_Dispatch/model.py
@@ -19,14 +19,16 @@ model.number_of_lines = Param(within = NonNegativeIntegers)
 
 model.indexes_time_periods = RangeSet(1, 
                                       model.number_of_time_periods)
-model.indexes_generation_units = RangeSet(1, 
+model.indexes_time_periods_except_first = RangeSet(2, 
+                                                   model.number_of_time_periods)
+model.indexes_generating_units = RangeSet(1, 
                                           model.number_of_generating_units)
 model.indexes_nodes = RangeSet(1, 
                                model.number_of_nodes)
 model.indexes_lines = RangeSet(1, 
                                model.number_of_lines)
 
-model.generating_units_cost = Param(model.indexes_generation_units,
+model.generating_units_cost = Param(model.indexes_generating_units,
                                     within = NonNegativeReals)
 model.demand = Param(model.indexes_lines,
                      model.indexes_time_periods,
@@ -59,6 +61,71 @@ def total_generating_cost(model):
 model.OBJ = Objective(rule = total_generating_cost)
 
 # Constraints
+
+def lower_bound_generating_power(model,
+                                 generating_unit,
+                                 time_period):
+    constraint = model.power_generating_units[generating_unit, time_period] >= model.lower_bound_power[generating_unit]
+    return(constraint)
+model.lower_bound_generating_power_constraint = Constraint(model.indexes_generating_units,
+                                                           model.indexes_time_periods,
+                                                           rule = lower_bound_generating_power)
+
+def upper_bound_generating_power(model,
+                                 generating_unit,
+                                 time_period):
+    constraint = model.power_generating_units[generating_unit, time_period] <= model.upper_bound_power[generating_unit]
+    return(constraint)
+model.upper_bound_generating_power_constraint = Constraint(model.indexes_generating_units,
+                                                           model.indexes_time_periods,
+                                                           rule = upper_bound_generating_power)
+
+#def power_balance(model,
+#                  node,
+#                  time_period):
+#    constraint = sum() == model.demand[node, time_period] + sum() - sum()
+#    return(constraint)
+#model.power_balance_constraint = Constraint(model.indexes_nodes,
+#                                            model.indexes_time_periods,
+#                                            rule = power_balance)
+
+def lower_bound_flow(model,
+                     line,
+                     time_period):
+    constraint = model.flow[line, time_period] >= - model.maximum_flow[line, time_period]
+    return(constraint)
+model.lower_bound_flow_constraint = Constraint(model.indexes_lines,
+                                               model.indexes_time_periods,
+                                               rule = lower_bound_flow)
+
+def upper_bound_flow(model,
+                     line,
+                     time_period):
+    constraint = model.flow[line, time_period] <= model.maximum_flow[line, time_period]
+    return(constraint)
+model.upper_bound_flow_constraint = Constraint(model.indexes_lines,
+                                               model.indexes_time_periods,
+                                               rule = upper_bound_flow)
+
+def lower_bound_ramping(model,
+                        generating_unit,
+                        time_period):
+    constraint = model.power_generating_units[generating_unit, time_period] - model.power_generating_units[generating_unit, time_period - 1] >= model.lower_bound_ramping[generating_unit]
+    return(constraint)
+model.lower_bound_ramping_constraint = Constraint(model.indexes_generating_units,
+                                                  model.indexes_time_periods_except_first,
+                                                  rule = lower_bound_ramping)
+
+def upper_bound_ramping(model,
+                        generating_unit,
+                        time_period):
+    constraint = model.power_generating_units[generating_unit, time_period] - model.power_generating_units[generating_unit, time_period - 1] <= model.upper_bound_ramping[generating_unit]
+    return(constraint)
+model.upper_bound_ramping_constraint = Constraint(model.indexes_generating_units,
+                                                  model.indexes_time_periods_except_first,
+                                                  rule = upper_bound_ramping)
+
+
 
 
 
